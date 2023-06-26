fb0e6778ae45442fadeb83b9d227fc87468a9582
Kevin de Heer <kevin@weconnect.nl>
2023-06-26 15:48:41 +0200

    Added post curl request

--- src/ActionHandler/HackathonPluginHandler.php
+++ src/ActionHandler/HackathonPluginHandler.php
@@ -47,9 +47,15 @@ class HackathonPluginHandler implements ActionHandlerInterface
         $url = 'https://test-hackathon-plugin.requestcatcher.com/test';
 
         $curl = curl_init();
+        $fields = array(
+            'field_name_1' => 'Value 1',
+            'field_name_2' => 'Value 2',
+            'field_name_3' => 'Value 3'
+        );
+        $fields_string = http_build_query($fields);
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
         $data = curl_exec($curl);
         curl_close($curl);
 
