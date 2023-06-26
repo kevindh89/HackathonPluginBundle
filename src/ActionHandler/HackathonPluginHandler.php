ebb4417013892f94e2c8cab424f88c42ccc9a29c
Kevin de Heer <kevin@weconnect.nl>
2023-06-26 16:01:32 +0200

    Post json using the hackathon plugin handler and prevent it from being returned

--- src/ActionHandler/HackathonPluginHandler.php
+++ src/ActionHandler/HackathonPluginHandler.php
@@ -52,10 +52,13 @@ class HackathonPluginHandler implements ActionHandlerInterface
             'field_name_2' => 'Value 2',
             'field_name_3' => 'Value 3'
         );
-        $fields_string = http_build_query($fields);
+        $json_string = json_encode($fields);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, TRUE);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json_string);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true );
+
         $data = curl_exec($curl);
         curl_close($curl);
 
